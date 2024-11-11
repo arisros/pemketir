0a67a9131a7cc9a26eb2503876f7da7b8d71c27a
Aris Jirat <arisjiratkurniawan@gmail.com>
2024-11-11 20:40:50 +0700

    use env

--- koneksi.php
+++ koneksi.php
@@ -1,23 +1,17 @@
 <?php
-// ini juga kudu diliat ente naro dimana database server ente ada dirumah siapa?
-$host = "localhost";
-// sesuain sama username db ente
-$user = "root";
-// sesuain sama password db ente
-$pass = "";
-// kudunya masih sama kalo ikutin doc
-$dbName = "db_pemketir";
+include("libs/env.php");
+
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$dbName = getenv("DB_NAME");
 $conn = mysqli_connect($host, $user, $pass);
 
 if (!$conn) {
   die("failed connect mysql" . mysqli_connect_error());
 }
 
-echo "success connect db! <br>";
-
 $sql = mysqli_select_db($conn, $dbName);
 if (!$sql) {
   die("failed connect db " . mysqli_connect_error());
 }
-
-echo "success connect db";
